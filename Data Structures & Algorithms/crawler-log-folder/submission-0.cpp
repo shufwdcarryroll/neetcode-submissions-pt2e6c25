1b7ed7d38bbceb4bce8fa116772fe905bf451f52
Shu <shumohan@umich.edu>
2026-05-11 15:43:09 -0400

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+
+        for (string log : logs) {
+            if (log == "../") {
+                if (depth > 0) depth--;
+            } 
+            else if (log != "./") {
+                depth++;
+            }
+        }
+
+        return depth;
+    }
+};
